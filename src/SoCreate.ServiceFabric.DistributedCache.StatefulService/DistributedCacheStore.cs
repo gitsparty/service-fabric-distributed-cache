66f26577768f1ff19b522b7bbaaa3007ea9b81ce
Nagendra Singh <nas@microsoft.com>
2019-09-02 23:32:18 -0700

    Parameterize cache size

--- src/SoCreate.ServiceFabric.DistributedCache.StatefulService/DistributedCacheStore.cs
+++ src/SoCreate.ServiceFabric.DistributedCache.StatefulService/DistributedCacheStore.cs
@@ -1,13 +1,31 @@
-﻿using System.Fabric;
+﻿using System;
+using System.Fabric;
 
 namespace SoCreate.ServiceFabric.DistributedCache.StatefulService
 {
     internal sealed partial class DistributedCacheStore : DistributedCacheStoreService
     {
+        private int _maxCacheSizeInMegaBytes = 1500;
+
         public DistributedCacheStore(StatefulServiceContext context)
             : base(context, (message) => ServiceEventSource.Current.ServiceMessage(context, message))
-        { }
+        {
+            var configurationPackage = context.CodePackageActivationContext.GetConfigurationPackageObject("Config");
 
-        protected override int MaxCacheSizeInMegabytes => 1500;
+            var value = configurationPackage.Settings.Sections["CacheConfig"].Parameters["MaxCacheSizeInMegabytes"].Value;
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                _maxCacheSizeInMegaBytes = (int)Convert.ChangeType(value, typeof(int));
+            }
+        }
+
+        protected override int MaxCacheSizeInMegabytes
+        {
+            get
+            {
+                return _maxCacheSizeInMegaBytes;
+            }
+        }
     }
 }
